cdb04cfb7f2ad986fcdaf95fd7aaf7d3fd9a3b66
lalyos <lalyos@yahoo.com>
2023-05-02 14:33:10 +0200

    add COLOR and BODY envs

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ cat > /var/www/html/index.html <<EOF
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
-<body>
-    <h1>${TITLE}</h1>
-    todo ...
+<body bgcolor="${COLOR:=gray}">
+    <h1>${TITLE:=Welcome}</h1>
+    ${BODY:=Please use TITLE/COLOR/BODY envs}
 </body>
 </html>
 EOF
