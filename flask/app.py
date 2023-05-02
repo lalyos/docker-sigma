d0c645f68dd914dc64e8e1f74dc86af7da1c29d7
lalyos <lalyos@yahoo.com>
2023-05-02 16:09:32 +0200

    use flask templates

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import render_template
+import os 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "<p>CoffeBreak for lalyos [flask]</p>"
+
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html',
+                            title=os.getenv("TITLE", default="Welcome"), 
+                            color=os.getenv("COLOR", default="gray"))
