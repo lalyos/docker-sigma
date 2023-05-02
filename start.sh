bfc3dd230b09454a1dfe9d6d7ff3dca4cc02cc9e
lalyos <lalyos@yahoo.com>
2023-05-02 14:24:09 +0200

    use heredoc for real html

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo ${TITLE:=Welcome} > /var/www/html/index.html
+cat > /var/www/html/index.html <<EOF
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>${TITLE}</h1>
+    todo ...
+</body>
+</html>
+EOF
 
 nginx -g "daemon off;"
