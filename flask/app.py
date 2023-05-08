e729f4c8326858458b039968d35c7651acd0c624
lalyos <lalyos@yahoo.com>
2023-05-08 13:52:52 +0200

    add /vip flask endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,45 @@
 from flask import Flask
 from flask import render_template
 import os 
+import psycopg2
+import json
 
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello_world():
-    return "<p>CoffeBreak for lalyos [flask]</p>"
+    return "<p>CoffeBreak for lalyos [flask] v4</p>"
+
+@app.route('/vip')
+def vip():
+
+    conn = psycopg2.connect(os.getenv("DB_URL"))
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Execute a SELECT statement to get all lines from the 'vip' table
+    cursor.execute("SELECT * FROM vip")
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Convert the results to a list of dictionaries
+    rows = []
+    for row in results:
+        rows.append({'Name': row[0], 'Age': row[1]})
+
+    # Convert the list of dictionaries to a JSON array
+    json_array = json.dumps(rows)
+
+    # Print the JSON array
+    print(json_array)
+
+    # Close the cursor and database connection
+    cursor.close()
+    conn.close()
+    return json_array
+
 
 @app.route('/')
 def hello(name=None):
